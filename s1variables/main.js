da3d11d55b79df82d504ab7ab5b40aa39381167d
Israel Salinas <israel.salinas.m@gmail.com>
2023-12-04 21:47:31 -0600

    first commit

---
+++ s1variables/main.js
@@ -0,0 +1,37 @@
+/*console.log("hola koders!");
+
+let a = "test A";
+
+const myFunction = () => {
+  console.log(a);
+  let b = "test B";
+  if (4 > 3) {
+    let c = "test C";
+    console.log(c);
+  }
+};
+
+
+
+myFunction();
+
+console.log(b);*/
+
+var a = "test"; /*declaración de variable*/
+a = "test a"; /*reasignación*/
+
+console.log(a);
+
+let b = "test";
+b = "test b";
+console.log(b);
+
+/*
+const c = "test";
+c = "test C";
+console.log(c);
+*/
+var foo = "foo"; /*declaración de variable*/
+var foo = "baz"; /*redeclaración de variable*/
+
+console.log(foo);
---
+++ s2arrowFunctions/main.js
@@ -0,0 +1,37 @@
+const addTwoNumbers = (firstNumber, secondNumber) => {
+  let result = firstNumber + secondNumber;
+  return result;
+};
+/*let firstAddition = addTwoNumbers(7,15)
+let secondAddition = addTwoNumbers(2,6)*/
+let firstAddition = addTwoNumbers(7, 15);
+
+//console.log(secondAddition);
+
+/*
+    Crear una función que reciba un número y me indique si es par o non
+    - Todo número par, devuelve un residuo de 0 al ser dividido entre 2
+    - Si el residuo es diferente de 0, entonces el número no es par
+
+    1.- tomar el número a evaluar
+    2.- divir el número entre 2
+    3.- evaluar el residuo
+        - Si el residuo es igual a 0, el número es par
+        - Si el residuo es diferente de 0, el número es impar
+    4.- entregar la indicación de si el número es par o non
+*/
+
+const isOddOrEven = (numberToEvaluate) => {
+  return numberToEvaluate % 2 === 0 ? "El número es par" : "El número es impar";
+};
+
+let firstEvaluation = isOddOrEven(7);
+let secondEvaluation = isOddOrEven(24);
+console.log(firstEvaluation);
+console.log(secondEvaluation);
+
+prompt("Ingresa un número");
+
+/*
+    Crear una función que reciba un string, cuente la cantidad de caracteres que tiene, y si tiene menos de 10 caracteres, indique que el string es corto, si tiene 10 o más indique que el string es largo
+    */
