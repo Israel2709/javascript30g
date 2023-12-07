beb9884c759253484a5e983aa8b54fcaee93da8e
Israel Salinas <israel.salinas.m@gmail.com>
2023-12-06 21:01:53 -0600

    ejercicios

--- ejerciciosStringMethods/main.js
+++ ejerciciosStringMethods/main.js
@@ -12,3 +12,29 @@
     input => "Merengue", "e"
     output => la letra "e" se usa 3 veces en la palabra "Merengue"
 */
+
+const findCharOcurrences = (originalString, char) => {
+  let expression = new RegExp(char, "g"); /*  /e/g    */
+  let ocurrencesCount = originalString.match(expression).length;
+  return `la letra ${char} usa ${ocurrencesCount} veces en la palabra "${originalString}"`;
+};
+
+let firstSearch = findCharOcurrences("Merenge", "n");
+console.log(firstSearch);
+
+let secondSearch = findCharOcurrences("Extraordinario", "r");
+console.log(secondSearch);
+
+let myRegex = new RegExp("e", "g");
+
+const createNewWord = (originalWord, limit) => {
+  let result = "";
+  for (let i = 0; i < limit; i++) {
+    let char = originalWord[i];
+    result += char;
+  }
+  return result;
+};
+
+let firstWord = createNewWord("enésimo", 4);
+console.log(firstWord);
