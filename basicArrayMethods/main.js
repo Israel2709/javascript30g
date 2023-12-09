9114d59647503a14a0c5a2a6ffd5e1341c2ee6af
Israel Salinas <israel.salinas.m@gmail.com>
2023-12-08 21:05:22 -0600

    .

---
+++ basicArrayMethods/main.js
@@ -0,0 +1,11 @@
+let people = [
+  ["Israel Salinas Martínez", 34, "israel@kodemia.mx"],
+  ["Naomi López Puertos", 25, "naomi@kodemia.mx"],
+  ["Charles Silva", 28, "charles@kodemia.mx"],
+  ["David Moranchel", 29, "david@kodemia.mx"],
+];
+/*
+    1.- Necesito recibir el nombre completo, edad y correo de una persona, y devolver un objeto que contenga esa información
+    2.- Tomando en cuenta la lista "people", necesito una nueva lista que contenga la información de cada persona pero agrupada en objetos
+    3.- Necesito poder ordenar una lista de objetos que representen personas con base en su edad, de forma descendente
+*/
