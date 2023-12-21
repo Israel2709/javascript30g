533b5074160ff2b66862de4f6cd9b7632e647820
Israel Salinas <israel.salinas.m@gmail.com>
2023-12-20 20:50:24 -0600

    ejercicio creación de dom

---
+++ domCreation copy/js/main.js
@@ -0,0 +1,6 @@
+/*
+  1.- Activar el formulario para poder agregar usuarios ( solo se pide el nombre completo)
+  2.- Activar el botón "eliminar". Este botón debe eliminar al usuario al que corresponde
+  3.- Activar el botón "ordenar por nombre". Este botón debe ordenar de forma alfabéticamente ascendente la lista de usuarios
+  4.- Activar el filtro por nombre: Debe filtrar la lista de usuarios conforme se escribe algo en el input
+  */
